b208e9213b2f3a048370206d3f7f1febbafc1eb1
zijadlicina <zijadlicina1@gmail.com>
2022-05-14 15:37:33 +0200

    modified error sql

--- full_app/config/db.js
+++ full_app/config/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("userms", "root", "", {
+const db = new Sequelize("products", "root", "", {
    host: "localhost",
    dialect: "mysql"
    //port: 3306
@@ -21,15 +21,6 @@ db.users = User;
 db.products = Product; 
 db.orders = Order;
 db.branches = Branch;
-
-const User = require('../models/User')(db, Sequelize);
-const Product = require('../models/Product')(db, Sequelize);
-const Branch = require("../models/Branch")(db, Sequelize);
-const Order = require("../models/Order")(db, Sequelize);
-db.user = User;
-db.product = Product; 
-db.user = Branch;
-db.product = Order; 
 
 db.sync( () => console.log(`Kreirane tabele i uneseni podaci!`));
 
